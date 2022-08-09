69d31b4b371b1f05e339b8e1e2c6d63affa7506a
Marcelo Najar <marcelo.snajar@gmail.com>
2022-08-09 17:33:11 -0400

    Added API of DOG CEO and fetched into the dropdown list

--- template/js/main.js
+++ template/js/main.js
@@ -1,1 +1,20 @@
 
+const BREED_DOGS = 'https://dog.ceo/api/breeds/list/all'
+
+const select  = document.querySelector('.breeds')
+
+fetch(BREED_DOGS)
+  .then(res => res.json())
+  .then(data => {
+    const breedObject = data.message // turn the message into an object
+    const breedsArray = Object.keys(breedObject) //turn the object into an array
+    //console.log(breedsArray)
+    
+    for (let i = 0; i < breedsArray.length; i++) {
+      const option = document.createElement('option')
+      option.value = breedsArray[i]
+      option.innerText = breedsArray[i]
+      select.appendChild(option)
+    }
+    
+  })
